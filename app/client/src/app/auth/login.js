689f52eb894f5e2cc59608a3361f638bb95f95a4
Martins Zeltins <martinszeltins@gmail.com>
2021-11-19 16:17:40 +0200

    Redirect to index after successful login

--- app/client/src/app/auth/login.js
+++ app/client/src/app/auth/login.js
@@ -1,10 +1,13 @@
 import Auth from '/@/api/auth'
+import router from '/@/routes.js'
 
 export function useLogin() {
     async function login(username, password) {
         const { data } = await Auth.login(username, password)
 
         localStorage.setItem('saskaitisana_user', JSON.stringify(data))
+
+        router.push({ name: 'index' })
     }
 
     return {
